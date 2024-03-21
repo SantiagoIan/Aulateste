942600f5413ad80c46ca0f7497563a47be7d32d5
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-03-21 14:15:25 -0300

    Update idade.c

--- Idade/idade.c
+++ Idade/idade.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+	
+int main() 
+{
+    int idade, sub, ano=2024;
+
+    printf ("Digite sua idade:");
+    scanf ("%d", &idade);
+    
+    sub= ano-idade;
+
+    printf ("\nvoce nasceu em: %d", sub);
+		return 0;
+}
 
