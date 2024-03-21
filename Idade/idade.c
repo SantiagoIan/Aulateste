3a52ae0359156a56bfa2443ba6ea2773dbd9acfd
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-03-21 14:16:06 -0300

    Update idade.c

--- Idade/idade.c
+++ Idade/idade.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 	
 int main() 
 {
@@ -10,6 +13,6 @@ int main()
     sub= ano-idade;
 
     printf ("\nvoce nasceu em: %d", sub);
+    
 		return 0;
 }
-
