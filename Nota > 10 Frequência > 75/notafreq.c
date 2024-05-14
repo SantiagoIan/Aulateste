732815df10afa15886c03d83dac4c3ac1783dc6b
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-05-14 15:52:34 -0300

    Update notafreq.c

--- Nota > 10 Frequência > 75/notafreq.c
+++ Nota > 10 Frequência > 75/notafreq.c
@@ -3,13 +3,12 @@
 int main() {
     float nota, frequencia;
 
-    // Solicita a nota e a frequência do aluno
     printf("Digite a nota do aluno: ");
     scanf("%f", &nota);
+    
     printf("Digite a frequencia do aluno: ");
     scanf("%f", &frequencia);
 
-    // Verifica se o aluno está aprovado
     if (nota > 10 && frequencia > 75) {
         printf("O aluno está aprovado!\n");
     } else {
