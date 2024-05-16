993317bb4e206265ecca4874f533421294e8ae50
Ian <101881756+SantiagoIan@users.noreply.github.com>
2024-05-15 21:07:10 -0300

    Create valorenome.c

---
+++ Incrementar Porcentagem/valorenome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    float valor, valor_corrigido;
+
+    printf("Digite o nome: ");
+    scanf("%s", nome);
+    printf("Digite o valor: ");
+    scanf("%f", &valor);
+    
+    if (valor > 5000) {
+        // Acrescenta 10% ao valor
+        valor_corrigido = valor * 1.10;
+    } else {
+        // Acrescenta 5% ao valor
+        valor_corrigido = valor * 1.05;
+    }
+
+    printf("Nome: %s\n", nome);
+    printf("Valor digitado: R$%.2f\n", valor);
+    printf("Valor corrigido: R$%.2f\n", valor_corrigido);
+
+    return 0;
+}
